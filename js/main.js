c84f20ed000a4835e57e67546631609156ddc038
Dvořák Dominik <dvorak@ohdeer.cz>
2014-07-15 15:52:24 +0200

    Functions init, alert

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-$(document).ready(function(){
-    $('#box').click(function(){
-        if($(this).attr('class') == 'bodyBlack'){
-            $(this).removeClass('bodyBlack');
-            $(this).addClass('bodyWhite');
-        }
-        else{
-            $(this).removeClass('bodyWhite');
-            $(this).addClass('bodyBlack');
-        }
-    });
-});
+$(document).ready(init());
+
+Function init(){
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+//PHONEGAP FUNCTIONS
+// onDeviceReady
+Function onDeviceReady(){
+	ShowNativeMessage("zprava",false,"Nadpis","Pokračujte");	
+}
+
+// notifications
+Function ShowNativeMessage(message,callback,title,buttonText){
+	navigator.notifications.alert(
+		message, 	//zprava
+		callback, 	//callback
+		title, 		//titulek
+		buttonText 	//nazev tlacitka
+	);	
+}
